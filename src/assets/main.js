22c5e7bf71a8d64e3310d202a7c6098dbbb72463
tomrep <tomasrepisky123@gmail.com>
2017-08-08 09:35:36 +0200

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -12,12 +12,12 @@ function guess() {
         attempt.value = Number(attempt.value) +1;
     }
     if(getResults(input.value)) {
-        setMessage("You win! :)");
+        setMessage("You win!");
         showAnswer(true);
         showReplay();
     } else {
         if(attempt.value >= 10) {
-            setMessage("You lose! :(");
+            setMessage("You lose!");
             showAnswer(false);
             showReplay();
         } else {
