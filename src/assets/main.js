0da67af786faa2ab5f9af5f576434e70427b6a87
tomrep <tomasrepisky123@gmail.com>
2017-08-08 09:43:21 +0200

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -77,9 +77,9 @@ function showAnswer(result){
     let code = document.getElementById('code');
     code.innerHTML = answer.value;
     if(result) {
-        code.className = 'success';
+        code.className += ' success';
     } else {
-        code.className = 'failure';
+        code.className += ' failure';
     }
 }
 
